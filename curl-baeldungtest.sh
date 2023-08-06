646196891a367f22fe44874dad4466165e76d96f
Pro Arhant <provungshu@gmail.com>
2023-08-06 21:34:58 +1000

    Another test for an expected successful CI build

--- curl-baeldungtest.sh
+++ curl-baeldungtest.sh
@@ -1,4 +1,4 @@
-#Expected syntax
-#curl --head https://www.baeldungtest.com
-#Introduce syntax error
-curl --head https://www.baeldungtest.com -Y SyntaxErrorTestForJenkinsBuild
+#The below curl command is an expected syntax for a successful CI build on PR.
+curl --head https://www.baeldungtest.com
+#Introduce syntax error like the one below for a faild CI build on PR.
+#curl --head https://www.baeldungtest.com -Y SyntaxErrorTestForJenkinsBuild
